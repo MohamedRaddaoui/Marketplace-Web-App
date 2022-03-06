4ae4fec84f8f75185c78a1df152eef75b947e8a6
xZeOs <mohamedraddaoui05@gmail.com>
2022-03-06 12:01:10 +0100

    Setting LogIn Page

--- src/app/template/login/login.component.ts
+++ src/app/template/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css','fonts/icomoon/style.css','css/bootstrap.min.css']
 })
 export class LoginComponent implements OnInit {
 
