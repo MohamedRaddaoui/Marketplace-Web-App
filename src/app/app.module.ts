955219f984120cd7cca5402c847c4050b33d6985
xZeOs <mohamedraddaoui05@gmail.com>
2022-03-06 12:35:46 +0100

    Setting home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './template/login/login.component';
 import { SignupComponent } from './template/signup/signup.component';
+import { HomeComponent } from './template/home/home.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
