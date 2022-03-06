34ab4112c02a610a35063bb7d4b459dbab6b51ee
xZeOs <mohamedraddaoui05@gmail.com>
2022-03-06 12:29:01 +0100

    Setting SignUp Page

--- src/app/template/signup/signup.component.ts
+++ src/app/template/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css','css/bootstrap.min.css','fonts/icomoon/style.css']
 })
 export class SignupComponent implements OnInit {
 
