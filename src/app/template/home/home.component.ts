65f10dfec9d83b7cb56e35ad8fb45ee9873488fe
xZeOs <mohamedraddaoui05@gmail.com>
2022-03-11 18:20:28 +0100

    Home Page Editing

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -11,5 +11,27 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  cutomercount:number = 0 ;
+  productcount:number = 0 ;
+  transactioncount:number = 0 ;
+
+  customercountstop:any = setInterval(() => {
+    this.cutomercount++;
+     if(this.cutomercount == 500){
+       clearInterval(this.customercountstop);
+     }
+  },1)
+  productcountstop:any = setInterval(() => {
+    this.productcount++;
+     if(this.productcount == 600){
+       clearInterval(this.productcountstop);
+     }
+  },1)
+  transactioncountstop:any = setInterval(() => {
+    this.transactioncount++;
+     if(this.transactioncount == 700){
+       clearInterval(this.transactioncountstop);
+     }
+  },1)
 
 }
